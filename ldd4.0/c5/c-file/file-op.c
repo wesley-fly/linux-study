2743125b47d7860c23d416a4e7b748058bd1dac4
Xiaofan@Ubuntu <lixiaofan0122@gmail.com>
2019-07-29 15:35:00 +0800

    compelete ch 5 of ldd4.0

---
+++ ldd4.0/c5/c-file/file-op.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define LENGTH 100
+
+int main(void)
+{
+	FILE *fd;
+	char str[LENGTH];
+
+	fd = fopen("hello.txt", "w+");
+	if(fd) {
+		fputs("Hello World", fd);
+		fclose(fd);
+	}
+
+	fd = fopen("hello.txt", "r");
+	fgets(str, LENGTH, fd);
+
+	printf("%s\n", str);
+
+	fclose(fd);
+}
---
+++ ldd4.0/c5/list-sysfs/list-sysfs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Block devices
+for i in /sys/block/*/dev /sys/block/*/*/dev
+do
+	if [ -f $i ]
+	then
+		MAJOR=$(sed 's/:.*//' < $i)
+		MINOR=$(sed 's/.*://' < $i)
+		DEVNAME=$(echo $i | sed -e 's@/dev@@' -e 's@.*/@@')
+		echo /dev/$DEVNAME b $MAJOR $MINOR
+		#mknod /dev/$DEVNAME b $MAJOR $MINOR
+	fi
+done
+
+#Char devices
+for i in /sys/bus/*/devices/*/dev /sys/class/*/*/dev
+do
+        if [ -f $i ]
+        then
+                MAJOR=$(sed 's/:.*//' < $i)
+                MINOR=$(sed 's/.*://' < $i)
+                DEVNAME=$(echo $i | sed -e 's@/dev@@' -e 's@.*/@@')
+                echo /dev/$DEVNAME c $MAJOR $MINOR
+                #mknod /dev/$DEVNAME c $MAJOR $MINOR
+        fi
+done
